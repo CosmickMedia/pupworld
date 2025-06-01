f1f12469a0222f3fbaa31982b5f4b60fbea7ca41
Carley Kuehner <ckuehner@cosmicktechnologies.com>
2025-05-31 22:58:05 -0400

    initial commit of original bark theme

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+class BARK_THEME {
+	function __construct() {
+		add_action( 'after_setup_theme', [ $this, 'after_setup_theme' ] );
+		add_action( 'init', [ $this, 'register_nav_menus' ] );
+		//add_action( 'init', [ $this, 'register_acf_blocks' ] );
+	}
+
+	function after_setup_theme() {
+		add_theme_support( 'menus' );
+	}
+
+	function register_nav_menus() {
+		register_nav_menus( [ 
+			'quick' => 'Quick Menu',
+			'main' => 'Main Menu',
+			'footer' => 'Footer Menu',
+			'admin' => 'Admin Menu'
+		] );
+	}
+
+	function register_acf_blocks() {
+		register_block_type( __DIR__ . '/blocks/testimonial' );
+	}
+}
+
+$BARK_THEME = new BARK_THEME();
+
+require_once ( dirname( __FILE__ ) . '/load.php' );
