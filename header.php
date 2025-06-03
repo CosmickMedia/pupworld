00911ef24bfba3a7494b18e42e933df9567b527a
Carley Kuehner <ckuehner@cosmicktechnologies.com>
2025-06-03 00:20:15 -0400

    Update header.php

--- header.php
+++ header.php
@@ -22,32 +22,6 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="primaryOffcanvas" aria-labelledby="primaryOffcanvasLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="primaryOffcanvasLabel"><?php bloginfo( 'name' ); ?></h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <?php
-                wp_nav_menu( array(
-                    'theme_location' => 'main',
-                    'menu_id'        => 'primary-menu-mobile',
-                    'container'      => false,
-                    'menu_class'     => 'navbar-nav justify-content-end flex-grow-1 pe-3',
-                    'fallback_cb'    => false,
-                ) );
-                ?>
-                <div class="social-icons mt-3">
-                    <a href="#" class="text-light me-3"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="text-light me-3"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="text-light"><i class="fab fa-instagram"></i></a>
-                </div>
-                <div class="business-info text-light mt-3">
-                    <div class="business-name fw-bold mb-1"><?php bloginfo( 'name' ); ?></div>
-                    <div class="header-phone"><i class="fas fa-phone"></i> <a href="tel:260-710-9103" class="text-light text-decoration-none">260.710.9103</a></div>
-                </div>
-            </div>
-        </div>
 
         <div class="collapse navbar-collapse d-none d-lg-flex" id="primary-menu-collapse">
             <?php
