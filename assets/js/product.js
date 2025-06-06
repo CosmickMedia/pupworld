938360f6675ec27c83dd379cab68c38540a96ccc
Carley Kuehner <43360628+starleycarley@users.noreply.github.com>
2025-06-06 17:44:15 -0400

    Fix product gallery slider initialization

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    if (typeof Swiper !== 'undefined') {
-        new Swiper('.product-gallery-swiper', {
+( function () {
+    if ( typeof Swiper !== 'undefined' ) {
+        new Swiper( '.product-gallery-swiper', {
             loop: true,
             slidesPerView: 1,
             spaceBetween: 10,
             pagination: {
                 el: '.swiper-pagination',
-                clickable: true
+                clickable: true,
             },
             navigation: {
                 nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
-        });
+                prevEl: '.swiper-button-prev',
+            },
+        } );
     }
-});
+} )();
