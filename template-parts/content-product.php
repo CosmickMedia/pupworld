384ebad909bd6707e2617cdf2d2a37c2999d1ee4
Carley Kuehner <43360628+starleycarley@users.noreply.github.com>
2025-06-07 11:19:11 -0400

    Use WooCommerce content template for single product

--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -13,6 +13,13 @@ get_template_part( 'template-parts/section-start', 'woo', [
 // WooCommerce page header with breadcrumb and title.
 get_template_part( 'template-parts/woocommerce', 'header' );
 
-get_template_part( 'template-parts/product-details' );
+// Load the detailed single product layout if available.
+if ( function_exists( 'wc_get_template_part' ) ) {
+    // This allows WooCommerce templates under the theme's /woocommerce folder
+    // to override the single product layout (e.g. content-single-product.php).
+    wc_get_template_part( 'content', 'single-product' );
+} else {
+    get_template_part( 'template-parts/product-details' );
+}
 
 get_template_part( 'template-parts/section-end', 'woo' );
