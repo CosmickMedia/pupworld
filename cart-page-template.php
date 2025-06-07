3d73ae3b30b398c3c821b848a9ce2bf167348375
Carley Kuehner <43360628+starleycarley@users.noreply.github.com>
2025-06-07 14:53:52 -0400

    Update cart and checkout templates to use block content

--- cart-page-template.php
+++ cart-page-template.php
@@ -6,8 +6,10 @@ get_header();
 ?>
 <main id="primary" class="site-main">
     <div class="container my-5" style="max-width:960px;">
-        <h1 class="page-title text-center mb-4"><?php the_title(); ?></h1>
-        <?php echo do_shortcode('[woocommerce_cart]'); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <h1 class="page-title text-center mb-4"><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
     </div>
 </main>
 <?php
